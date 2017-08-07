be0e319a7ac5126293015f36d22c044c094ab65c
rachel <rache882@gmail.com>
2017-08-07 20:56:02 +1000

    init commit

--- app.js
+++ app.js
@@ -10,12 +10,34 @@ app.use(express.static('public'));
 
 //routes
 app.get('/', function(req,res) {
-  
+
   }
 )
 
+app.get('/users/new', function(req,res) {
 
+  }
+)
 
+app.post('/users', function(req,res) {
+
+  }
+)
+
+app.get('/dashboard', function(req,res) {
+
+  }
+)
+
+app.post('/users/id/votes', function(req,res) {
+
+  }
+)
+
+app.delete('/users/id/votes', function(req,res) {
+
+  }
+)
 
 app.listen(PORT, function() {
   console.log(`Server now listening on port ${PORT}`);
