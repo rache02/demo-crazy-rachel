1b0d7523181dcd2a8399e9175ba78b80ab9799e4
rachel <rache882@gmail.com>
2017-08-08 18:03:28 +1000

    created dbcall function and query all function"

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ const PORT = 3000;
 var express = require('express');
 var app = express();
 var request = require('request');
+var mongo = require('mongodb')
+var MongoClient = mongo.MongoClient
+var assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/test';
+
+//create test
+function dbCall(query) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Connented to Database");
+    query(db);
+    db.close();
+  });
+}
+
+function getAllUsers(db) {
+  db.users.find();
+}
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -10,14 +30,13 @@ app.use(express.static('public'));
 
 //routes
 app.get('/', function(req,res) {
-
-  }
-)
+  dbCall(getAllUsers)
+})
 
 app.get('/users/new', function(req,res) {
 
   }
-)
+);
 
 app.post('/users', function(req,res) {
 
@@ -41,4 +60,5 @@ app.delete('/users/id/votes', function(req,res) {
 
 app.listen(PORT, function() {
   console.log(`Server now listening on port ${PORT}`);
-}
+  }
+)
