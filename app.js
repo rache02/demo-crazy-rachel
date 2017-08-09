39fade300d924a656d7b09f1c1289dd3220d3540
rachel <rache882@gmail.com>
2017-08-09 10:13:26 +1000

    there is a problem

--- app.js
+++ app.js
@@ -4,19 +4,30 @@ var app = express();
 var request = require('request');
 var mongo = require('mongodb')
 var MongoClient = mongo.MongoClient
+var Server = mongo.Server;
 var assert = require('assert');
+var session = require('express-session')
 
 // Connection URL
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://localhost:27017/democrazy';
 
-//create test
-function dbCall(query) {
+function dbCall(query, code) {
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     console.log("Connented to Database");
     query(db);
     db.close();
   });
+}
+
+function getUser(db,code) {
+  cursor = db.collection('users').find({code: code})
+  result = cursor.toArray()
+  if (result.length > 0) {
+    console.log("it worked");
+  } else {
+    console.log("somethings wrong");
+  }
 }
 
 function getAllUsers(db) {
@@ -27,36 +38,50 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+}))
 
 //routes
 app.get('/', function(req,res) {
-  dbCall(getAllUsers)
+  res.render('login');
+});
+
+app.post('/login', function(req,res) {
+  var inputCode = req.query['code'];
+
+  dbCall(getUser, inputCode)
 })
 
-app.get('/users/new', function(req,res) {
+app.post('/session', function(req,res) {
 
-  }
-);
+});
 
-app.post('/users', function(req,res) {
+app.get('/users/:id', function(req,res) {
 
-  }
-)
+});
 
-app.get('/dashboard', function(req,res) {
+app.patch('/users/:id', function(req,res) {
 
-  }
-)
+});
+
+app.get('/users', function(req,res) {
+
+});
 
 app.post('/users/:id/votes', function(req,res) {
 
-  }
-)
+});
 
 app.delete('/users/:id/votes', function(req,res) {
 
-  }
-)
+});
+
+app.delete('/session', function(req,res) {
+
+});
 
 app.listen(PORT, function() {
   console.log(`Server now listening on port ${PORT}`);
